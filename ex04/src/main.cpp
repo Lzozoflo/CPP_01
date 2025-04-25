55252b7a792f1a60fbc8250761cb1ad6fc092b80
fcretin <fcretin@student.42lyon.fr>
2025-04-25 18:15:55 +0200

    ex04 fini

--- ex04/src/main.cpp
+++ ex04/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fcretin <fcretin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/25 11:06:45 by fcretin           #+#    #+#             */
-/*   Updated: 2025/04/25 17:41:05 by fcretin          ###   ########.fr       */
+/*   Updated: 2025/04/25 18:07:26 by fcretin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,23 +19,53 @@
 #define BLUE  "\033[34m"
 #define YELLOW "\033[33m"
 
+
+
+std::string replaceString(std::string original, const std::string& toReplace, const std::string& replacement)
+{
+	size_t pos = 0;
+
+	// .find find the first occurrence of "toReplace"
+	// == npos its when ".find" say no more "toReplace" here
+	while ((pos = original.find(toReplace, pos)) != std::string::npos)
+	{
+		// Replace the substring
+		original.erase(pos, toReplace.length());
+		original.insert(pos, replacement);
+
+		// add the len of replacement for the infinity loop
+		pos += replacement.length();
+	}
+
+	return original;
+}
+
 int main(int ac, char **av)
 {
 	if (ac != 4){
 		std::cerr << RED << "Not the required number of arguments" << RESET << std::endl;
-		return (1);}
+		return (1);
+	}
+
+	// fd of the file
 	std::ifstream	ifs(av[1]);
+	if (!ifs.is_open()){
+		std::cerr << RED << "Error on: " << av[1] << RESET << std::endl;
+		return (1);
+	}
 	std::string		line;
 	std::string		str;
 
+	// get line by line
 	while (std::getline(ifs , line))
 	{
-		str += line;
+		//replace and add line by line + a \n
+		str += replaceString(line, av[2], av[3]);
 		str += "\n";
 	}
 	std::cout << str << std::endl;
 
 
 	ifs.close();
-	return 0;
+	return (0);
 }
